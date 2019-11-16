c8a65f303815afa07f2cbb8fbc78cf55c66c8924
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-11-16 23:52:56 +0200

    Add Plugins folder and Wifi test script
    * AndroidManifest for os permissions
    * Test.cs with some unfinished Wifi interfacing logic

---
+++ Assets/Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text foo = GetComponent<Text>();
+        //foo.text = "FOOBAR";
+
+        using (var actClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            var activity = actClass.GetStatic<AndroidJavaObject>("currentActivity");
+            if (activity == null)
+            {
+                foo.text = "NULL";
+                return;
+            }
+            List<String> bar = activity.Call<List<String>>("getWifiScanResults");
+            if (bar == null)
+            {
+                foo.text = "FUCK";
+                return;
+            }
+            foreach (String name in bar)
+            {
+                foo.text += name + " - ";
+            }
+
+        }
+    }
+}
