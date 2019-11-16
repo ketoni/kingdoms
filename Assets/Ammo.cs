1318ad779ded8404151a6f2da00bc212462bc8de
tohtoriteppo <tohtoriteppo@hotmail.com>
2019-11-16 12:56:44 +0200

    Folder structure, first cannon logic

---
+++ Assets/Ammo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] float lift = 0.01f;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(0, -lift));
+    }
+}
---
+++ Assets/Cannon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] GameObject ammoPrefab;
+
+    private float maxDistance;
+    private bool dragging;
+    private Vector2 pressPos;
+    private Resolution resolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolution = Screen.currentResolution;
+        maxDistance = resolution.width / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            dragging = true;
+            pressPos = Input.mousePosition;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            Shoot(Input.mousePosition);
+        }
+
+    
+    }
+    private void Shoot(Vector2 releasePos)
+    {
+        Vector2 direction = pressPos - releasePos;
+        direction = direction.normalized;
+        float distance = Vector2.Distance(pressPos, releasePos);
+        GameObject ammo = Instantiate(ammoPrefab, transform);
+        //set size
+        float scale = Mathf.Max(distance, 1);
+        Vector2 size = new Vector2(scale, scale);
+        ammo.transform.localScale = size;
+        //set mass
+        Rigidbody2D rb = ammo.GetComponent<Rigidbody2D>();
+        rb.mass = scale;
+        //shoot
+        Vector2 force = direction * distance * rb.mass;
+        rb.AddForce(force);
+    }
+}
