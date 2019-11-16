535ee9da763dae861293e5aa13d795446dcf4e08
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-11-17 00:20:02 +0200

    Add random spin to projectiles

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -117,6 +117,7 @@ public class Cannon : NetworkBehaviour
         rb.mass = bulletScale;
         Vector2 force = bulletDirection * rb.mass * bulletPower;
         rb.AddForce(force);
+        rb.AddTorque((Random.value - 0.5f) * 50f);
         ammo.GetComponent<Ammo>().Shoot();
     }
 
